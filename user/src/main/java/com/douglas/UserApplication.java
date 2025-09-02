a6303c86d6d05759172d177896429b6244dcb945
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2025-09-01 23:34:57 -0300

    Entidade usuário criado

--- user/src/main/java/com/douglas/UserApplication.java
+++ user/src/main/java/com/douglas/UserApplication.java
@@ -1,4 +1,4 @@
-package com.douglas.user;
+package com.douglas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ user/src/main/java/com/douglas/entity/User.java
@@ -0,0 +1,5 @@
+package com.douglas.entity;
+
+public class User {
+
+}
