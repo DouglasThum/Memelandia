85487d037f2fc97011b295ef519a313a859b0c83
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2025-09-06 03:01:21 -0300

    CategoriaMeme finalizado - Adicionado anotação EnableJPARepository

--- categoriameme/src/main/java/com/douglas/CategoriamemeApplication.java
+++ categoriameme/src/main/java/com/douglas/CategoriamemeApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.douglas.repository.CategoriaMemeRepository;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableJpaRepositories(basePackageClasses = CategoriaMemeRepository.class)
 public class CategoriamemeApplication {
 
 	public static void main(String[] args) {
