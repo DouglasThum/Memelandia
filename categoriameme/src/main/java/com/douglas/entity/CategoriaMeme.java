0808f4865c48dd4a6708288fe808780fbd0a8c8a
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2025-09-08 00:21:43 -0300

    Adição de padrão de data em categoria Meme

--- categoriameme/src/main/java/com/douglas/entity/CategoriaMeme.java
+++ categoriameme/src/main/java/com/douglas/entity/CategoriaMeme.java
@@ -2,6 +2,8 @@ package com.douglas.entity;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,6 +23,7 @@ public class CategoriaMeme {
     @Column(nullable = false)
     private String descricao;
 
+    @JsonFormat(pattern = "dd-MM-yyyy")
     @Column(nullable = false)
     private LocalDate dataCadastro;
 
